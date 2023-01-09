dde8785e277119fbcab2a4b88d6b8c4c5f0120e1
KazuhideOka <kazu1de0ka@gmail.com>
2023-01-09 14:29:26 +0900

    add develop files

---
+++ develop/chrome_forWindows.bat
@@ -0,0 +1,3 @@
+cd /d "C:\Program Files (x86)\Google\Chrome\Application"
+chrome.exe --allow-file-access-from-files --allow-file-access --allow-cross-origin-auth-prompt --disable-web-security --user-data-dir="D:\kazuhide-oka\projects\localChrome"
+pause
