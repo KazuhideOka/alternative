51553c3a6536ff0a35d9296c3ccc0db872cd383c
KazuhideOka <kazu1de0ka@gmail.com>
2023-01-15 18:57:01 +0900

    update launch bat

--- develop/chrome_forWindows_2.bat
+++
@@ -1,3 +0,0 @@
-cd /d "C:\Program Files\Google\Chrome\Application"
-chrome.exe --allow-file-access-from-files --allow-file-access --allow-cross-origin-auth-prompt --disable-web-security --user-data-dir="C:\Users\kazu1\Documents\Chrome"
-pause
--- develop/chrome_forWindows_x86.bat
+++ develop/chrome_forWindows_x86.bat
@@ -1,3 +1,3 @@
-cd /d "C:\Program Files\Google\Chrome\Application"
+cd /d "C:\Program Files (x86)\Google\Chrome\Application"
 chrome.exe --allow-file-access-from-files --allow-file-access --allow-cross-origin-auth-prompt --disable-web-security --user-data-dir="%USERPROFILE%\Documents\Chrome"
 pause
