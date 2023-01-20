ca698f64003de357b3f9888d2cd695378d0cb798
KazuhideOka <kazu1de0ka@gmail.com>
2023-01-20 22:51:20 +0900

    add progress manager

---
+++ js/app/progress/progressManager.js
@@ -0,0 +1,16 @@
+class ProgressManager extends GlobalManager
+{
+    constructor()
+    {
+        super("progressManager");
+
+        this.progress = 0;
+    }
+
+    setup()
+    {
+
+    }
+}
+
+new ProgressManager();
